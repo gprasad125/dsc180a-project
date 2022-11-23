fb7b502db0565c2cca3a85e214816e4b057bac37
Gokul Prasad <gokulprasad125@gmail.com>
2022-11-22 17:43:01 -0800

    added try/except to run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from src.data.make_dataset import *
 from src.viz.visualization import *
 
-import pandas as pd
 import sys
 
 def main(targets):
@@ -13,10 +12,13 @@ def main(targets):
         inpath = 'data/test/test.csv'
         outpath = 'data/test/test_out.csv'
 
-    make_dataset(inpath, outpath)
-    fig_1, fig_2, fig_3 = make_visuals(outpath)
-
-
+    try:
+        make_dataset(inpath, outpath)
+        fig_1, fig_2, fig_3 = make_visuals(outpath)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
 
 
 if name == '__main__':
