dda3c1e3332f03dfed2511a41763f1d134c58b57
Gokul Prasad <gokulprasad125@gmail.com>
2022-11-22 16:25:09 -0800

    added test data & submission.json

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,11 +1,11 @@
-# imports 
+# imports
 import pandas as pd
 import numpy as np
 
 
 ############################################
 
-# User Defined Cleaning Functions 
+# User Defined Cleaning Functions
 
 def clean_state(state):
     if pd.isnull(state) == False:
@@ -13,13 +13,13 @@ def clean_state(state):
             state = state[1:-1]
     else:
         state = "Unknown"
-    
+
     return state
 
 def get_year(bday):
-    
+
     if not pd.isnull(bday):
-        
+
         return int(bday[0:4])
 
 ############################################
@@ -27,14 +27,14 @@ def get_year(bday):
 def make_dataset():
 
     # read in filepath RELATIVE to run.py
-    filepath = "data/raw/sampleData.csv"
+    filepath = "data/raw/raw.csv"
     df = pd.read_csv(filepath)
 
     # drop columns, NaN values
     df.drop(columns = ["userid_str", "status_id_str", "id"], inplace = True, errors = "ignore")
     df.dropna(inplace = True)
 
-    # add or reshape columns using lambdas & UDFs 
+    # add or reshape columns using lambdas & UDFs
     df["year"] = df["birth"].apply(lambda x: int(x[0:4]))
     df["term_partisanship"] = df["term_partisanship"].apply(clean_state)
     df["term_state"] = df["term_state"].apply(clean_state)
@@ -42,5 +42,5 @@ def make_dataset():
     df["age_when_posted"] = df["posted"] - df["year"]
 
     # convert cleaned DataFrame to csv
-    new_filepath = "data/out/tweets_cleaned.csv"
+    new_filepath = "data/out/out.csv"
     df.to_csv(new_filepath)
